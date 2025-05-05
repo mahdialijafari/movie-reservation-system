89c87adc05b4b993e2cfc15d77ad7acf40cac8bc
mahdialijafari <sedmahdialijafari@gmail.com>
2025-05-06 00:43:41 +0330

    completing show time routes

---
+++ api/Routes/showtime.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { isAdmin } from '../Middlewares/isAdmin.js'
+import { create, getAll, getByMovieGrouped, getOne, remove, update } from '../Controllers/showtimeCn.js'
+
+const showTimeRouter = express.Router()
+
+showTimeRouter.route('/').get(getAll).post(isAdmin,create)
+showTimeRouter.route('/by-movie-goroup').get(getByMovieGrouped)
+showTimeRouter.route('/:id').get(getOne).patch(isAdmin,update).delete(isAdmin,remove) 
+
+export default showTimeRouter
